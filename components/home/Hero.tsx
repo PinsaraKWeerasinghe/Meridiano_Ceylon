1b6241f597de652079e0d1ef61e07cbaf6897d33
Pinsara <pinsarakweerasinghe@gmail.com>
2026-04-04 18:39:39 +0530

    Updated home page

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,13 +4,13 @@ import { HeroSlideshow } from "@/components/home/HeroSlideshow";
 
 export function Hero() {
   return (
-    <section className="relative -mt-[var(--navbar-h)] overflow-hidden bg-forest px-4 pb-16 pt-[calc(var(--navbar-h)+4rem)] text-cream sm:px-6 sm:pb-24 sm:pt-[calc(var(--navbar-h)+6rem)]">
+    <section className="relative -mt-[var(--navbar-h)] overflow-hidden bg-forest text-cream">
       <HeroSlideshow slides={heroSlides} intervalMs={6500} />
       <div
         className="absolute inset-0 z-[2] bg-gradient-to-b from-black/45 via-black/38 to-black/50"
         aria-hidden
       />
-      <div className="relative z-10 mx-auto max-w-3xl text-center">
+      <div className="relative z-10 mx-auto max-w-3xl px-4 pb-16 pt-[calc(var(--navbar-h)+4rem)] text-center sm:px-6 sm:pb-24 sm:pt-[calc(var(--navbar-h)+6rem)]">
         <p className="text-sm font-medium uppercase tracking-[0.2em] text-goldMint">
           Luxury Sri Lanka Tours
         </p>
